4050e8fb6d1643bdbac39cdb9dcf87b6b1ee6b6b
ajadiolatunde <ajadiolatunde@gmail.com>
2019-09-20 22:58:15 +0100

    test url

--- author/tests/test_models.py
+++ author/tests/test_models.py
@@ -1,14 +1,20 @@
 from django.test import TestCase
 from author.models import  Author
+from django.test.client import  Client
 from author import urls,views
 from django.urls import path
 
 
 class AuthorTest(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        kwargs = {'fname':'olatunde','lname':'ajadi'}
+    # @classmethod
+    # def setUpTestData(cls):
+    #     kwargs = {'fname':'olatunde','lname':'ajadi'}
+    #     Author.objects.create(**kwargs)
+
+    def setUp(self):
+        self.c = Client()
+        kwargs = {'fname': 'olatunde', 'lname': 'ajadi'}
         Author.objects.create(**kwargs)
 
 
@@ -26,5 +32,11 @@ class AuthorTest(TestCase):
         url = author.get_absolute_url()
         print ("-----",url)
         self.assertEquals(url,'/a/1')
+
+    def test_url_response(self):
+        response = self.c.get('/a/1')
+        print (response.__dict__)
+        self.assertEquals(response.status_code,200)
+
 
 
--- author/views.py
+++ author/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def authorlist(request):
-    pass
+def authorlist(request,id):
+    print ("---------",id)
+    return  HttpResponse("Cool")
